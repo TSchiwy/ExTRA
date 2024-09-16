48cc12f233ce24169ec52f540c15e2141c028e0a
Tom Schiwy <tom.schiwy@web.de>
2024-09-16 14:36:47 +0200

    ordner

---
+++ ExTRApack/__init__.py
@@ -0,0 +1,18 @@
+
+
+
+
+
+import numpy as np
+
+
+
+from .astrometry import *
+from .RVsolo import *
+from .RVcomb import *
+from .hipparcos import *
+from .astrometry import *
+from .read import *
+from .likeli import *
+
+print("ExTRA correctly imported")
---
+++ ExTRApack/hipparcos.py
@@ -0,0 +1,109 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+#This function calculates the new abscissa residual if you change a parameter compared to the hipparchos solution.
+#For example if parallax_hipparchos=40mas , parallax_model=10mas, you will get a parameter_residual=-30mas.
+#This is done for all the standard model parameters.
+#The value of hipp_derivation, is given in the IAD of hipparchos.Namely IA3-IA7
+#They are different in the first and the second reduction
+#we will be working with the second reduction ONLY
+
+#the first old_residual is the hipparchos_residual given in mas
+def abs_res(old_res,parameter_model,parameter_hipp,hipp_derivation):
+    parameter_residual=parameter_model-parameter_hipp
+    
+    new_res=old_res
+    for i in range(5):
+        new_res=new_res-hipp_derivation[i]*parameter_residual[i]
+    
+    return new_res
+
+
+def JD_hip(A4,A7): #finds JD of measurement
+    frac=A7/A4
+    epoch=frac+1991.25
+    JD=2451545.0+(epoch-2000.0)*365.25 #JD for standard epoch J2000
+    return JD
+
+
+def scanangle_hip(A3,A4): #finds the scanangle between NORTH and RGC(reference great circle) in radians
+    angle=np.arctan2(A4,A3)
+    angle=angle
+    return angle
+
+#the scanangle opens in the direction of RA
+
+
+
+
+##############################################
+
+
+
+
+def rotation_counterclockwise(x,y,theta):
+    x_new=+np.cos(theta)*x-np.sin(theta)*y
+    y_new=+np.sin(theta)*x+np.cos(theta)*y
+    #y_new=-1*y_new
+    return x_new,y_new
+    
+    
+
+
+def hip_2d(A3,A4,A8,A9): #rotates hip measurement into Dec RA* system
+    """rotates a hip measurement into Ra* ,Dec, returns x,x_err,y,y_err
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    """
+    scanangle=scanangle_hip(A3,A4)
+    x=rotation_counterclockwise(A8,0,scanangle)[0]
+    y=rotation_counterclockwise(A8,0,scanangle)[1]
+    x_err=rotation_counterclockwise(A9,0,scanangle)[0]
+    y_err=rotation_counterclockwise(A9,0,scanangle)[1]
+    #print(np.degrees(scanangle))
+    return np.array([x,x_err,y,y_err])
+
+
+#This plots the dimension hipparchos DIDNT measure in, its just nice for plots but thats it
+#inputs: x0,y0 positon of measurement,x_err,y_err error in direction,get this from hip measurement,
+#size= length of line
+def plot_not(x0,x_err,y0,y_err,size=1,co="r",s=10,linecolor="lightgrey"):
+#this part just adds the rotation of the error so they can be plot in the right direction
+
+    norm=1/((x_err**2 +y_err**2)**0.5)
+    x1=x0-y_err*norm*size
+    y1=y0+x_err*norm*size
+    x2=x0+y_err*norm*size
+    y2=y0-x_err*norm*size
+#then plots a line for each measurement in size and angle of the error    
+    plt.scatter(x0,y0,color=co,s=s)
+    plt.plot([x1,x2],[y1,y2],color=linecolor)
+    
+
+
+
+
+
+#This plots the dimension hipparchos measured
+#inputs: x0,y0 positon of measurement,x_err,y_err error in direction,get this from hip measurement,
+def plot_real(x0,x_err,y0,y_err,co="r",s=1,linecolor="lightgrey"):
+#this part just adds the rotation of the error so they can be plot in the right direction
+
+    x1=x0-x_err
+    y1=y0-y_err
+    x2=x0+x_err
+    y2=y0+y_err
+#then plots a line for each measurement in size and angle of the error    
+    plt.scatter(x0,y0,color=co,s=s)
+    plt.plot([x1,x2],[y1,y2],color=linecolor)
